053fec6ed57db57f4bc64870e6afd06614d14c46
Emilio Osoria <team@curiosityled.com>
2026-04-08 01:47:46 +0000

    File changes

--- src/pages/MVPAIInsights.jsx
+++ src/pages/MVPAIInsights.jsx
@@ -3,12 +3,15 @@
  * Wrapped in FullAuthProvider so components using @/components/useAuth work.
  */
 import { AuthProvider as FullAuthProvider } from "@/components/useAuth";
+import { ContextProviders } from "@/components/contexts/ContextProviders";
 import AIInsightsDashboard from "./AIInsightsDashboard";
 
 export default function MVPAIInsights() {
   return (
     <FullAuthProvider>
-      <AIInsightsDashboard />
+      <ContextProviders>
+        <AIInsightsDashboard />
+      </ContextProviders>
     </FullAuthProvider>
   );
 }
